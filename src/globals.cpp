adc99594b98073fcfe7f0a67db0b860307119786
matomehsu <matomehsu@gmail.com>
2023-10-10 17:13:46 -0400

    added ports
    (michael on matome's computer)

--- src/globals.cpp
+++ src/globals.cpp
@@ -4,19 +4,20 @@
 pros::Motor FL(16, pros::E_MOTOR_GEARSET_06, true, pros::E_MOTOR_ENCODER_DEGREES); 
 pros::Motor ML(12, pros::E_MOTOR_GEARSET_06, true, pros::E_MOTOR_ENCODER_DEGREES); 
 pros::Motor BL(4, pros::E_MOTOR_GEARSET_06, true, pros::E_MOTOR_ENCODER_DEGREES); 
-pros::Motor FR(17, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_DEGREES);
-pros::Motor MR(14, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_DEGREES); 
-pros::Motor BR(7, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor FR(19, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor MR(7, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_DEGREES); 
+pros::Motor BR(14, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_DEGREES);
 
-pros::Motor Cata1(1, pros::E_MOTOR_GEARSET_36, true, pros::E_MOTOR_ENCODER_DEGREES);
-pros::Motor Cata2(2, pros::E_MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor Cata1(9, pros::E_MOTOR_GEARSET_36, true, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor Cata2(11, pros::E_MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_DEGREES);//?
 
 pros::Controller controller(pros::E_CONTROLLER_MASTER);
 
 pros::IMU inertial(3);
-pros::Rotation leftRot(10);
-pros::Rotation rightRot(11);
+pros::Rotation leftRot(17);
+pros::Rotation rightRot(15);
 
+//think these are ok
 pros::ADIDigitalOut wings('A');
 pros::ADIDigitalOut intake('B');
 
