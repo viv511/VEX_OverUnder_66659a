054bdf4da77c17958e70b9284f69ccebbdcbeb19
Vivek M <66559749+viv511@users.noreply.github.com>
2023-11-04 18:57:21 -0400

    Updated ports

--- src/globals.cpp
+++ src/globals.cpp
@@ -1,21 +1,21 @@
 #include "main.h"
 #include "globals.h"
 
-Motor FL(20, E_MOTOR_GEARSET_06, true, E_MOTOR_ENCODER_DEGREES); //
-Motor ML(3, E_MOTOR_GEARSET_06, true, E_MOTOR_ENCODER_DEGREES); //
-Motor BL(10, E_MOTOR_GEARSET_06, true, E_MOTOR_ENCODER_DEGREES); //
-Motor FR(15, E_MOTOR_GEARSET_06, false, E_MOTOR_ENCODER_DEGREES); //
-Motor MR(17, E_MOTOR_GEARSET_06, false, E_MOTOR_ENCODER_DEGREES); //
-Motor BR(16, E_MOTOR_GEARSET_06, false, E_MOTOR_ENCODER_DEGREES); //
+Motor FL(19, E_MOTOR_GEARSET_06, true, E_MOTOR_ENCODER_DEGREES); //
+Motor ML(20, E_MOTOR_GEARSET_06, true, E_MOTOR_ENCODER_DEGREES); //
+Motor BL(6, E_MOTOR_GEARSET_06, true, E_MOTOR_ENCODER_DEGREES); //
+Motor FR(11, E_MOTOR_GEARSET_06, false, E_MOTOR_ENCODER_DEGREES); //
+Motor MR(13, E_MOTOR_GEARSET_06, false, E_MOTOR_ENCODER_DEGREES); //
+Motor BR(15, E_MOTOR_GEARSET_06, false, E_MOTOR_ENCODER_DEGREES); //
 
-Motor Cata1(7, E_MOTOR_GEARSET_36, true, E_MOTOR_ENCODER_DEGREES);
-Motor Cata2(12, E_MOTOR_GEARSET_36, false, E_MOTOR_ENCODER_DEGREES);
+Motor Cata1(3, E_MOTOR_GEARSET_36, true, E_MOTOR_ENCODER_DEGREES);
+Motor Cata2(8, E_MOTOR_GEARSET_36, false, E_MOTOR_ENCODER_DEGREES);
 
 Controller controller(E_CONTROLLER_MASTER);
 
-IMU inertial(2);
-Rotation leftRot(18);
-Rotation rightRot(11);
+IMU inertial(0);
+Rotation leftRot(0);
+Rotation rightRot(0);
 
 //think these are ok
 ADIDigitalOut wings('A');
