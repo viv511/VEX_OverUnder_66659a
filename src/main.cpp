54c6a7f16a93ac0ae8fa8684b27b81a8f5ea4335
bn615 <125498021+bn615@users.noreply.github.com>
2024-04-15 12:44:20 -0400

    fixes

--- src/main.cpp
+++ src/main.cpp
@@ -88,11 +88,11 @@ void opcontrol() {
 		
 		if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_R1))
 		{
-			intake.move_voltage(12000);
+			intake.move_voltage(-12000);
 		}
 		else if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_L1))
 		{
-			intake.move_voltage(-12000);
+			intake.move_voltage(12000);
 		}
 		else
 		{
